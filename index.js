f1d04a169f4397b09cd1a177be56f3bbc4438ca1
akashmalla07 <akashmalla07@gmail.com>
2016-05-24 13:51:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,6 +109,6 @@ io.on('connection',function(socket){
 
 
 
-http.listen(3000,function(){
-    console.log('server listening on port 3000');
+http.listen(8000,function(){
+    console.log('server listening on port 8000');
 })
